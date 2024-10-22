LineageOS/android_device_google_zuma

Add initial Lineage common makefiles
Change-Id: I8136e9f0df40ca3bf96dbfd2453275481178cdaa

BoardConfig-common.mk
@@ -433,3 +433,5 @@ BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
 # pick up library for cleaning digital car keys on factory reset
 -include vendor/google_devices/gs-common/proprietary/BoardConfigVendor.mk
+
+include device/google/zuma/BoardConfigLineage.mk

lineage_common.mk
@@ -1,0 +1,8 @@
+#
+# SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/zuma/overlay-lineage

jaiminkalariya/android_device_google_zumapro

BoardConfig-common.mk
@@ -435,3 +435,5 @@ BOARD_PVMFWIMAGE_PARTITION_SIZE := 0x00100000
 
 # pick up library for cleaning digital car keys on factory reset
 -include vendor/google_devices/gs-common/proprietary/BoardConfigVendor.mk
+
+include device/google/zumapro/BoardConfigLineage.mk

lineage_common.mk
@@ -1,0 +1,8 @@
+#
+# SPDX-FileCopyrightText: 2021-2024 The LineageOS Project
+# SPDX-FileCopyrightText: 2021-2024 The Calyx Institute
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += device/google/zumapro/overlay-lineage